35f9b2e8dcc63902384e7f2a8ce2951613017d5b
Steven Burris <stevenburris.jr@gmail.com>
2016-09-20 19:35:39 -0400

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -58,7 +58,7 @@ public class Main {
                         checkbox = " {Has been cleaned.} ";
                         System.out.println();
                     }
-                    System.out.printf("%s %s %s %s tanks of this type.", number, checkbox, tank3.tankNames, tank3.numberOfTanks);
+                    System.out.printf("%s %s %s - %s %s tanks.", number, checkbox, tank3.numberOfTanks, tank3.tankNames,tank3.category);
                     System.out.println();
                     System.out.println();
                 }
@@ -71,8 +71,8 @@ public class Main {
             System.out.println("2: Check/uncheck which tanks have been cleaned.");
             System.out.println("3: View a list of all tanks currently added to your hospital's inventory.");
             System.out.println("4: Remove a tank from your hospital's inventory list.");
-            System.out.println("5: Log Out");
-            System.out.println("6: Change the quantity of listed tanks.");
+            System.out.println("5: Change the quantity of listed tanks.");
+            System.out.println("6: Log Out");
 
             String option = scanner.nextLine();
 
@@ -100,7 +100,8 @@ public class Main {
                             checkbox = " {Has been cleaned.} ";
                             System.out.println();
                         }
-                        System.out.printf("%s %s %s %s tanks of this type.", number, checkbox, tank3.tankNames, tank3.numberOfTanks);
+                        System.out.printf("%s %s %s - %s %s tanks.", number, checkbox, tank3.numberOfTanks, tank3.tankNames,tank3.category);
+
                         System.out.println();
                         System.out.println();
                     }
@@ -114,15 +115,15 @@ public class Main {
                     break;
 
                 case "5":
-                    main(args);
-                    break;
-
-                case "6":
                     System.out.println("Which tank would you like to update the quantity of?");
                     int index = Integer.valueOf(scanner.nextLine());
                     System.out.println("What quantity would you like to have?");
                     int quant = Integer.valueOf(scanner.nextLine());
                     tanks.get(index - 1).numberOfTanks = quant;
+                    break;
+
+                case "6":
+                    main(args);
                     break;
 
                 default:
@@ -134,129 +135,32 @@ public class Main {
         }
     }
 
-    public static Tank createItem (String name) {
+    public static Tank createItem(String name) {
         System.out.println("Enter the name of your tank.");
         String tankName1 = scanner.nextLine();
         System.out.println("How many of these tanks do you have?");
         int tankQuant = scanner.nextInt();
         System.out.println("What type of tank is this?");
+        String tankType1 = scanner.nextLine();
         String tankType = scanner.nextLine();
         if (tankType.equalsIgnoreCase("ust")) {
             Tank tank = new Tank(tankName1, tankQuant, false, "UST");
             return tank;
-        }
-        else if (tankType.equalsIgnoreCase("ast")) {
+        } else if (tankType.equalsIgnoreCase("ast")) {
             Tank tank = new Tank(tankName1, tankQuant, false, "AST");
             return tank;
-        }
-        else if (tankType.equalsIgnoreCase("sbt")) {
+        } else if (tankType.equalsIgnoreCase("sbt")) {
             Tank tank = new Tank(tankName1, tankQuant, false, "SBT");
+            return tank;
+        } else if (tankType.equalsIgnoreCase("concrete")) {
+            Tank tank = new Tank(tankName1, tankQuant, false, "Concrete");
+            return tank;
+        } else if (tankType.equalsIgnoreCase("invisible")) {
+            Tank tank = new Tank(tankName1, tankQuant, false, "Invisible");
+            return tank;
+        } else {
+            System.out.println("That is not a valid type of tank.");
+            return createItem(name);
         }
-        else if (tankType.equalsIgnoreCase("concrete")) {
-
-        }
-
-        engineers.get(name).getTanks().add(tank);
-        System.out.println();
-        return tank;
-
     }
-
 }
-
-
-//        while (true) {
-//
-//
-//            ArrayList<Tank> tanks = engineers.get(name);
-//            if (tanks == null) {
-//                System.out.println("You haven't added any tanks yet.");
-//                tanks = new ArrayList<>();
-//                engineers.put(name, tanks);
-//                System.out.println();
-//            }
-//
-//            boolean isLoggedIn = true;
-//            while (isLoggedIn) {
-//
-//                for (int t = 0; t < tanks.size(); t++) {
-//                    Tank tank3 = tanks.get(t);
-//                    int number = t + 1;
-//                    String checkbox = " {Has not been cleaned.} ";
-//                    if (tank3.hasBeenCleaned) {
-//                        checkbox = " {Has been cleaned.} ";
-//                    }
-//                    System.out.printf("%s %s %s %s tanks of this type\n", number, checkbox, tank3.tankNames, tank3.numberOfTanks);
-//                }
-//                System.out.println();
-//                System.out.println("Here are your options:");
-//                System.out.println();
-//                System.out.println("1: Add tanks to your hospital's inventory.");
-//                System.out.println("2: Check/uncheck which tanks have been cleaned.");
-//                System.out.println("3: View a list of all tanks currently added to your hospital's inventory.");
-//                System.out.println("4: Remove a tank from your hospital's inventory list.");
-//                System.out.println("5: Log Out");
-//                System.out.println("6: Change the quantity of listed tanks.");
-//
-//                String option = scanner.nextLine();
-//
-//                switch (option) {
-//
-//                    case "1":
-//                        System.out.println("Enter the name of your tank.");
-//                        String tankName = scanner.nextLine();
-//                        Tank tank = new Tank(tankName, 1, false);
-//                        tanks.add(tank);
-//                        System.out.println();
-//                        break;
-//
-//                    case "2":
-//                        System.out.println("Which tank would you like to check off as cleaned or not yet cleaned?");
-//                        int i = Integer.valueOf(scanner.nextLine());
-//                        Tank tank2 = tanks.get(i - 1);
-//                        tank2.hasBeenCleaned = !tank2.hasBeenCleaned;
-//                        break;
-//
-//                    case "3":
-//                        for (int t = 0; t < tanks.size(); t++) {
-//                            Tank tank3 = tanks.get(t);
-//                            int number = t + 1;
-//                            String checkbox = " {Has not been cleaned.} ";
-//                            if (tank3.hasBeenCleaned) {
-//                                checkbox = " {Has been cleaned.} ";
-//                                System.out.println();
-//                            }
-//                            System.out.printf("%s %s %s %s tanks of this type.", number, checkbox, tank3.tankNames, tank3.numberOfTanks);
-//                            System.out.println();
-//                            System.out.println();
-//                        }
-//                        break;
-//
-//                    case "4":
-//                        System.out.println("Which tank would you like to remove?");
-//                        int removal = Integer.valueOf(scanner.nextLine());
-//                        tanks.remove(removal - 1);
-//                        break;
-//
-//                    case "5":
-//                        isLoggedIn = false;
-//                        break;
-//
-//                    case "6":
-//                        System.out.println("Which tank would you like to update the quantity of?");
-//                        int index = Integer.valueOf(scanner.nextLine());
-//                        System.out.println("What quantity would you like to have?");
-//                        int quant = Integer.valueOf(scanner.nextLine());
-//                        tanks.get(index - 1).numberOfTanks = quant;
-//
-//                        break;
-//
-//                    default:
-//                        System.out.println("You can't do that!");
-//                        break;
-//                }
-//            }
-//        }
-//    }
-//}
-
